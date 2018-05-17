804727d98a8d4e36f8dbacc3ec8e754e136a36a8
brokerstir <take1risk@gmail.com>
2018-05-17 14:03:06 -0500

    deletion

--- pdo_db_live.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=robertri_companycrm','robertri_bstir', '77!%TUuTFba');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
